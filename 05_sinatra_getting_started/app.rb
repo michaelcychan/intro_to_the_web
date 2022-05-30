aa5e55a02250b85f19c62fd223b5e4de5f42d372
Michael Chan <michaelcychan@Michaels-MacBook-Air.local>
2022-05-30 14:53:16 +0100

    view and route

--- 05_sinatra_getting_started/app.rb
+++ 05_sinatra_getting_started/app.rb
@@ -1,14 +1,23 @@
 require 'sinatra'
-require "sinatra/reloader"
+require 'sinatra/reloader'
 
 get '/' do
   "Hello"
 end
 
+# remember to add / in front of all routes
 get '/secret' do
   "Secret route"
 end
 
-get 'other_route' do
+get '/other_route' do
   "This is another route"
+end
+
+get '/new_route' do
+  'new route here!'
+end
+
+get '/cat' do
+  erb(:index)
 end
