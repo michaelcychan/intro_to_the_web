d5f0ec3465e0c79919823fd027bca7d81760958f
Michael Chan <michaelcychan@Michaels-MacBook-Air.local>
2022-06-01 15:50:10 +0100

    skinny controller

---
+++ Battle/lib/game.rb
@@ -0,0 +1,19 @@
+# lib/game.rb
+
+class Game
+  def initialize(player_1, player_2)
+    @players = [player_1, player_2]
+  end
+
+  def player_1
+    @players[0]
+  end
+
+  def player_2
+    @players[1]
+  end
+
+  def attack(player)
+    player.receive_damage
+  end
+end
