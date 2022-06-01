cc7a1af9c8b642a30a485ba13b3fd50ea83a9ec7
Michael Chan <michaelcychan@Michaels-MacBook-Air.local>
2022-06-01 15:08:07 +0100

    hit points

--- Battle/spec/features/attack_spec.rb
+++ Battle/spec/features/attack_spec.rb
@@ -6,4 +6,11 @@ feature 'Attacking' do
     click_button 'Attack!'
     expect(page).to have_content 'Charlotte attacked Mittens'
   end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack!'
+    expect(page).not_to have_content 'Mittens: 100HP'
+    expect(page).to have_content 'Mittens: 90HP'
+  end
 end
